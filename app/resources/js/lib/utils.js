bc563c7e0ee1c1eb7028ac72a3043e2030caed8b
PutuRifkiDy <puturifki56@gmail.com>
2025-03-27 13:50:31 +0800

    Konfigurasi File dan Setup Fonts

--- app/resources/js/lib/utils.js
+++ app/resources/js/lib/utils.js
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const STATUS = {
+    VERIFIED: 'verified',
+    REQUEST: 'request',
+    PENDING: 'pending',
+    REJECTED: 'rejected',
+};
+
+
