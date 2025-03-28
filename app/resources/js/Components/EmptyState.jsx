3d64796bf9256083dad70f76bde1acc90861a798
PutuRifkiDy <puturifki56@gmail.com>
2025-03-28 14:54:13 +0800

    Setup Components

---
+++ app/resources/js/Components/EmptyState.jsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+import { Link } from "@inertiajs/react";
+import {PiDatabase} from "react-icons/pi";
+export function EmptyState({ title, url, className }) {
+    return(
+        <Link
+            href={url}
+            className={cn('relative block w-full rounded-lg border-2 border-dashed border-muted-foreground p-12 text-center hover:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2')}
+        >
+            <PiDatabase className='mx-auto h-12 w-12 text-muted-foreground'/>
+            <span className="block mt-2 text-sm font-medium leading-relaxed tracking-tighter">
+                Create a new {title}
+            </span>
+        </Link>
+    );
+}
---
+++ app/resources/js/Components/GetStatusBadge.jsx
@@ -0,0 +1,29 @@
+import { STATUS } from "@/lib/utils";
+import { Badge} from "@/components/ui/badge";
+
+export function GetStatusBadge({ status })
+{
+    const {VERIFIED, REQUEST, PENDING, REJECTED} = STATUS;
+
+    let status, text;
+
+    switch(status){
+        case VERIFIED:
+            status = 'bg-green-500 hover:bg-green-600';
+            text = 'Verified';
+            break;
+        case REQUEST:
+            status = 'bg-indigo-500 hover:bg-indigo-600';
+            text = 'Request';
+            break;
+        case PENDING:
+            status = 'bg-yellow-500 hover:bg-yellow-600';
+            text = 'Pending';
+            break;
+        case REJECTED:
+            status = 'bg-red-500 hover:bg-red-600';
+            text = 'Rejected';
+            break;
+    }
+    return <Badge className={status}>{text}</Badge>;
+}
