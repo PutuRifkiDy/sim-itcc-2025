e94c6e295e770fdc5d92e82dbb2ec996d16bf8e2
PutuRifkiDy <puturifki56@gmail.com>
2025-03-31 21:21:07 +0800

    Membuat Migration dan Model Event Content Contact Person

---
+++ app/app/Models/EventContentContactPerson.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EventContentContactPerson extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'event_content_id',
+        'name',
+        'id_line',
+        'wa_number',
+    ];
+}
