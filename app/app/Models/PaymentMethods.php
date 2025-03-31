aa6d35e798fa3028d0229309e62983887bfd3f75
PutuRifkiDy <puturifki56@gmail.com>
2025-03-31 22:28:40 +0800

    Memperbaiki Model di Payment Methods

--- app/app/Models/PaymentMethods.php
+++ app/app/Models/PaymentMethods.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PaymentMethods extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'account_number',
+        'bank_name',
+        'recipient_name',
+    ];
 }
