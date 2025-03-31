42cb9d2f6e2dbf83207d43766dd2687ea885c73a
PutuRifkiDy <puturifki56@gmail.com>
2025-03-31 21:37:39 +0800

    Membuat Model dan Migration Merchandise

---
+++ app/app/Models/Merchandise.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Merchandise extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'price',
+        'image_path',
+        'description',
+        'batch_name',
+        'start_date',
+        'end_date',
+    ];
+}
