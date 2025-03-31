c9271fece2fcb65d74a8e9aa922ea329f1f9a352
PutuRifkiDy <puturifki56@gmail.com>
2025-03-31 13:32:08 +0800

    Membuat Model dan Migration TeamMembers

---
+++ app/app/Models/TeamMembers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TeamMembers extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'team_id',
+        'competition_registration_id',
+    ];
+}
