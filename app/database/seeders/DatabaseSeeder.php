7f4eb873e02290062523fd54373a8a6197fc67ff
PutuRifkiDy <puturifki56@gmail.com>
2025-04-01 14:55:19 +0800

    Membuat Seeder Competitions

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             CompetitionCategoriesSeeder::class,
-
+            CompetitionsSeeder::class
         ]);
     }
 }
