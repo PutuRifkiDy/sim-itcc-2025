1cb206de82405585996e38c590332c61d28d8f31
PutuRifkiDy <puturifki56@gmail.com>
2025-04-01 14:23:10 +0800

    Membuat Seeder Competition Category

---
+++ app/database/seeders/CompetitionCategoriesSeeder.php
@@ -0,0 +1,43 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\CompetitionCategory;
+use Illuminate\Database\Seeder;
+
+class CompetitionCategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $competition_categories = [
+            [
+                'category_name' => 'SD',
+            ],
+            [
+                'category_name' => 'SMP',
+            ],
+            [
+                'category_name' => 'SD/SMP',
+            ],
+            [
+                'category_name' => 'SMASMK',
+            ],
+            [
+                'category_name' => 'Mahasiswa',
+            ],
+            [
+                'category_name' => 'Umum',
+            ],
+            [
+                'category_name' => 'Mahasiswa/Umum',
+            ],
+        ];
+
+        foreach($competition_categories as $category) {
+            CompetitionCategory::create($category);
+        }
+
+    }
+}
