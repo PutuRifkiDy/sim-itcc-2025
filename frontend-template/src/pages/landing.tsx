c1ddb6cf3c9fe44a1e25f04643670f454f9a678f
Rama Devantara <runesong71@gmail.com>
2025-05-03 22:57:31 +0800

    `Added competition component layout and SVG icons`

--- frontend-template/src/pages/landing.tsx
+++ frontend-template/src/pages/landing.tsx
@@ -2,6 +2,7 @@ import HeroSection from "../Components/HeroSection";
 import AboutSection from "../Components/AboutSection";
 import OurSponsor from "../Components/OurSponsor";
 import Competition from "../Components/Competition";
+import Merch from "../Components/Merch";
 
 export function Landing() {
   return (
@@ -10,6 +11,7 @@ export function Landing() {
       <AboutSection />
       <OurSponsor />
       <Competition />
+      <Merch />
     </>
   );
 }
