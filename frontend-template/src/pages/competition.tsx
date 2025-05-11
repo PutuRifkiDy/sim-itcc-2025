3a474f8dcf103a284b938d3d04c6224e19b914cb
Najwa Tahir <najwatahir186@gmail.com>
2025-05-11 23:20:44 +0800

    Create Contact Person section on Semnas & Competition page

--- frontend-template/src/pages/competition.tsx
+++ frontend-template/src/pages/competition.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "../Components/Competition/About";
 import HeroSection from "../Components/Competition/Hero";
+import ContactPerson from "../Components/Competition/ContactPerson";
 
 export function Competition() {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <ContactPerson />
     </>
   );
 }
--- frontend-template/src/pages/semnas.tsx
+++ frontend-template/src/pages/semnas.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "../Components/Semnas/About";
 import HeroSection from "../Components/Semnas/Hero";
+import ContactPerson from "../Components/Semnas/ContactPerson";
 
 export function Semnas() {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <ContactPerson />
     </>
   );
 }
