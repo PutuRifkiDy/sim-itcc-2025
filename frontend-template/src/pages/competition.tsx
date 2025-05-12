9c6fdafb7c08a2b42d50c6268b9d864fc2fd6604
Najwa Tahir <najwatahir186@gmail.com>
2025-05-12 13:39:30 +0800

    Create Prize Category section on Competition page

--- frontend-template/src/pages/competition.tsx
+++ frontend-template/src/pages/competition.tsx
@@ -1,12 +1,14 @@
 import AboutSection from "../Components/Competition/About";
 import HeroSection from "../Components/Competition/Hero";
 import ContactPerson from "../Components/Competition/ContactPerson";
+import PrizeCategory from "../Components/Competition/PrizeCategory";
 
 export function Competition() {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <PrizeCategory />
       <ContactPerson />
     </>
   );
