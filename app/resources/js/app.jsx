6a021cfad890a134116338178464459d9f7a01ec
PutuRifkiDy <puturifki56@gmail.com>
2025-05-15 10:22:35 +0800

    Update: App.jsx

--- app/resources/js/app.jsx
+++ app/resources/js/app.jsx
@@ -4,6 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot, hydrateRoot } from 'react-dom/client';
+import { ThemeProvider } from './Components/ThemeProvider';
 
 const appName = import.meta.env.VITE_APP_NAME || 'ITCC-2025';
 
@@ -11,12 +12,12 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
-        if (import.meta.env.SSR) {
-            hydrateRoot(el, <App {...props} />);
-            return;
-        }
-
-        createRoot(el).render(<App {...props} />);
+            hydrateRoot(
+                el,
+                <ThemeProvider defaultTheme='dark' storageKey='current-theme'>
+                    <App {...props} />
+                </ThemeProvider>
+            );
     },
     progress: {
         color: '#4B5563',
