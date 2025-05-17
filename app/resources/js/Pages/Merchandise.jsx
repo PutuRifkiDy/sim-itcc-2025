2826bc399e654d085013376ebe7b9167ad55acd0
PutuRifkiDy <puturifki56@gmail.com>
2025-05-17 21:11:22 +0800

    Added: AOS in Modal Popup Merch

--- app/resources/js/Pages/Merchandise.jsx
+++ app/resources/js/Pages/Merchandise.jsx
@@ -4,16 +4,31 @@ import GuestLayout from "@/Layouts/GuestLayout";
 import { ArrowDownIcon } from "@heroicons/react/24/solid";
 import { Link, usePage } from "@inertiajs/react";
 import { motion } from 'framer-motion';
+import * as AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 function Merchandise({ ...props }) {
     const merchandise = usePage().props.merchandise;
     console.log('cek isi var', merchandise);
+
+    useEffect(() => {
+        AOS.init({
+            duration: 800,
+            once: false,
+            easing: 'ease-out-cubic',
+            offset: 100,
+            delay: 0,
+            mirror: false,
+            anchorPlacement: 'top-bottom',
+        });
+    }, []);
     return (
         <>
             <img
                 src="assets/images/bg_ornament.png"
                 alt="banner"
-                className="absolute z-0 w-auto object-center rounded-none h-screen object-cover hidden md:block top-0 opacity-70 dark:opacity-0"
+                className="absolute z-0 w-auto object-center rounded-none h-screen object-cover hidden md:block top-0 opacity-60 dark:opacity-0"
             />
             <div className="isolate min-h-screen md:mt-24">
                 <div className="relative md:px-24 px-10">
@@ -22,7 +37,7 @@ function Merchandise({ ...props }) {
                         aria-hidden="true"
                     >
                         <div
-                            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#0F114C] to-[#0F114C]/50 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#0F114C]/50 to-[#0F114C]/20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                             style={{
                                 clipPath:
                                     'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -40,13 +55,13 @@ function Merchandise({ ...props }) {
                                 repeat: Infinity,
                                 ease: "linear"
                             }}
-                            className="w-full h-full"
+                            className="w-full h-full animate-pulse"
                         >
                             <SideLeftCrookedCrossIcon />
                         </motion.div>
                     </div>
 
-                    <div className="absolute top-0 right-48 md:block hidden">
+                    <div className="absolute top-12 right-48 md:block hidden text-[#0F114C]">
                         <SideRightCrossIcon />
 
                     </div>
@@ -56,21 +71,21 @@ function Merchandise({ ...props }) {
                     </div>
 
                     <div className="flex md:flex-row flex-col justify-around">
-                        <img src="assets/images/merch_bundle.png" alt="" className="md:w-[446px] md:h-[468px] w-full h-auto object-contain" />
+                        <img src="assets/images/merch_bundle.png" alt="" className="md:w-[446px] md:h-[468px] w-full h-auto object-contain " data-aos="fade-up" data-aos-delay="100" />
                         <div className="flex flex-col items-center justify-center">
-                            <p className="text-[#0F114C] dark:text-[#0886BB] leading-[1.2em] text-[50px] tracking-[0.016em] uppercase font-bold text-center">
+                            <p className="text-[#0F114C] dark:text-[#0886BB] leading-[1.2em] text-[50px] tracking-[0.016em] uppercase font-bold text-center" data-aos="fade-up" data-aos-delay="100">
                                 MERCHANDISE bundle
                             </p>
-                            <p className="text-[18px] tracking-[0.02em] leading-[1.8em] md:w-[380px] w-full text-center mt-5 text-[#000000] font-light dark:text-white">
+                            <p className="text-[18px] tracking-[0.02em] leading-[1.8em] md:w-[380px] w-full text-center mt-5 text-[#000000] font-light dark:text-white" data-aos="fade-up" data-aos-delay="200">
                                 Get your Exclusive Merchandise Bundle at a special price — limited time only!
                             </p>
-                            <p className="text-[23px] font-medium text-center mt-10 text-[#0886BB] line-through decoration-2">
+                            <p className="text-[23px] font-medium text-center mt-10 text-[#0886BB] line-through decoration-2" data-aos="fade-up" data-aos-delay="300">
                                 Rp. 199.000
                             </p>
-                            <p className="text-[29px] font-medium text-center text-[#495190]">
+                            <p className="text-[29px] font-medium text-center text-[#495190]" data-aos="fade-up" data-aos-delay="300">
                                 Only Rp. 100.000
                             </p>
-                            <Button variant="blue" asChild size="lg" className="mt-5">
+                            <Button variant="blue" asChild size="lg" className="mt-5" data-aos="fade-up" data-aos-delay="300">
                                 <a href="#">Buy Bundle</a>
                             </Button>
                         </div>
@@ -80,7 +95,7 @@ function Merchandise({ ...props }) {
                         aria-hidden="true"
                     >
                         <div
-                            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#0F114C]/80 to-[#0F114C]/50 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#0F114C]/50 to-[#0F114C]/20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
                             style={{
                                 clipPath:
                                     'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
