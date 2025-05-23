a394f2aa87fe7903925a42651fb9574975124eef
PutuRifkiDy <puturifki56@gmail.com>
2025-05-23 15:13:28 +0800

    Refactored: Merchandise in Competition

--- app/resources/js/Pages/Merchandise.jsx
+++ app/resources/js/Pages/Merchandise.jsx
@@ -1,4 +1,4 @@
-import { SideLeftArrowLeftIcon, SideLeftCrookedCrossIcon, SideRightBlueDotIcon, SideRightCrossIcon } from "@/Components/IconGuest";
+import { SideLeftArrowLeftIcon, SideLeftCrookedCrossIcon, SideRightBlueDotIcon, SideRightCircleIcon, SideRightCrossIcon } from "@/Components/IconGuest";
 import { Button } from "@/Components/ui/button";
 import GuestLayout from "@/Layouts/GuestLayout";
 import { ArrowDownIcon } from "@heroicons/react/24/solid";
@@ -22,6 +22,22 @@ function Merchandise({ ...props }) {
             anchorPlacement: 'top-bottom',
         });
     }, []);
+
+    const formatMoney = (nominal) => {
+        return new Intl.NumberFormat('id-ID', {
+            style: 'currency',
+            currency: 'IDR',
+            minimumFractionDigits: 0,
+        }).format(new Number(nominal));
+    };
+
+    const formatDateInTimeLine = (tanggal) => {
+        return new Intl.DateTimeFormat('id', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+        }).format(new Date(tanggal));
+    };
     return (
         <>
             <img
@@ -112,137 +128,104 @@ function Merchandise({ ...props }) {
                         />
                     </div>
                 </div>
-                <div className="pt-36" id="card-merch">
-                    {merchandise.map((merchandise, idx) => (
-                        <div key={idx} className="" >
-                            <p>{merchandise.name}</p>
-                            <p>{merchandise.price}</p>
-                            <p>{merchandise.description}</p>
-                            <p>{merchandise.batch_name}</p>
-                            <p>{merchandise.start_date}</p>
-                            <p>{merchandise.end_date}</p>
+
+                {/* Competition Section */}
+                <section className="relative overflow-hidden py-16 md:py-24">
+                    <div className="text-center md:mb-10 mb-10  flex flex-col justify-center items-center">
+                        <h2 className="uppercase md:text-4xl text-2xl font-bold text-[#0F114C] md:tracking-[10px] tracking-[5px] dark:text-white" data-aos="fade-up">
+                            Our Merchandise
+                        </h2>
+                        <p className="mt-4 text-[#5E5E5E] font-rubik text-sm sm:text-base md:text-lg font-normal tracking-[0.02em] dark:text-gray-400 md:w-[600px] w-full" data-aos="fade-up" data-aos-delay="100">
+                            Support us and show your pride by getting our official merchandise.
+                            High-quality, stylish, and made just for you — wear it and be part of
+                            the community!
+                        </p>
+                    </div>
+                    {/* Content Container */}
+                    <div className="relative z-10 max-w-[1350px] mx-auto px-4 md:px-8">
+
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8 justify-items-center">
+                            {merchandise.map((merch, i) => (
+                                <div
+                                    key={i}
+                                    className="group relative w-full max-w-md overflow-hidden rounded-2xl bg-white shadow-lg transition-all duration-500 hover:shadow-2xl border-2 border-[#0F114C]/50 hover:border-[#0F114C] p-3 h-[500px]"
+                                    data-aos="fade-up"
+                                    data-aos-delay="100"
+                                >
+
+                                    {/* Background Game Image */}
+                                    <div
+                                        className="absolute inset-0 m-3 rounded-xl bg-cover bg-center transition-transform duration-500 group-hover:scale-110 bg-gradient-to-r from-[#0F114C]/100 to-[#00658F]"
+                                    />
+
+                                    {/* Static Dark Overlay for Mobile View Only */}
+                                    <div className="absolute inset-0 rounded-xl bg-black/60 block md:hidden" />
+
+                                    {/* Dark Overlay (only on desktop hover) */}
+                                    <div className="absolute inset-0 rounded-xl bg-gradient-to-t from-black/90 via-black/70 to-transparent opacity-0 transition-opacity duration-500 hidden md:block group-hover:opacity-100" />
+
+                                    {/* Content Container */}
+                                    <div className="relative h-full w-full flex flex-col items-center justify-center">
+
+                                        {/* Game Logo */}
+                                        <div className="absolute z-20 -top-1 left-1/2 -translate-x-1/2 md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
+                                            <div className="flex justify-center items-center rounded-full p-8 transition-all duration-500 md:group-hover:-translate-y-[140%]">
+                                                <img
+                                                    src="assets/images/landing/ITCC.png"
+                                                    alt="text"
+                                                    className="h-42 w-auto object-contain transition-all duration-500 md:group-hover:scale-50"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        {/* merchandise path gambar */}
+                                        <div className="absolute z-10 -top-1 left-1/2 -translate-x-1/2 md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
+                                            <div className="flex justify-center items-center rounded-full transition-all duration-500">
+                                                <img
+                                                    src="assets/images/merchandise/merchItcc.png"
+                                                    alt="text"
+                                                    className="transition-all duration-500 md:group-hover:scale-140"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        {/* Content (shown on mobile and on hover in desktop) */}
+                                        <div className="absolute bottom-0 left-0 right-0 flex flex-col items-center p-5 my-5 opacity-100 translate-y-0 md:opacity-0 md:translate-y-4 transition-all duration-500 md:group-hover:opacity-100 md:group-hover:translate-y-0 z-30">
+                                            <h3 className="text-center text-2xl font-bold mb-4 text-white">
+                                                {merch.name}
+                                                <div className="w-64 h-0.5 mt-2 bg-[#0F114C] mx-auto rounded-full"></div>
+                                            </h3>
+                                            <div className="flex flex-col gap-5 mb-2 items-center">
+                                                <div className="flex flex-col gap-1 items-center mb-10">
+                                                    <p className="text-[16px] font-medium text-white">{merch.batch_name}</p>
+                                                    <p className="text-[12px] font-normal text-white">{formatDateInTimeLine(merch.start_date)} - {formatDateInTimeLine(merch.end_date)}</p>
+                                                </div>
+                                                <div className="flex flex-col gap-1 items-center max-w-[300px] text-center">
+                                                    <p className="text-[16px] font-medium text-white">Pick-up Offline</p>
+                                                    <p className="text-[12px] font-normal text-white">
+                                                        Gedung Teknologi Informasi, Universitas Udayana, Jimbaran, Bali
+                                                    </p>
+                                                </div>
+                                                <p className="text-[16px] font-medium text-white">{formatMoney(merch.price)}</p>
+                                            </div>
+                                            <Button variant="blue" size="lg">
+                                                <Link
+                                                    href={route('register')}
+                                                    className=""
+                                                >
+                                                    Register Now
+                                                </Link>
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
+                    </div>
+                </section>
             </div>
         </>
     );
 }
 export default Merchandise;
 Merchandise.layout = (page) => <GuestLayout children={page} title="Merchandise" />;
-
-
-// import { Button } from '@/Components/ui/button';
-// import { ArrowDownIcon } from '@heroicons/react/24/solid';
-// import { Link, usePage } from '@inertiajs/react';
-
-// export default function Merch() {
-//     const auth = usePage().props.auth.user;
-//     return (
-//         <div>
-//             <header className="absolute inset-x-0 top-0 z-50">
-//                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-//                     <div className="flex lg:flex-1">
-//                         <Link href="/" className="-m-1.5 p-1.5 text-2xl font-black leading-relaxed tracking-tighter">
-//                             Plannify<span className="text-red-500">.</span>
-//                         </Link>
-//                     </div>
-//                     <div className="flex lg:hidden">
-//                         {auth ? (
-//                             <Link href={route('dashboard')} className="font-semibold leading-relaxed tracking-tighter text-foreground">
-//                                 Dashboard <span aria-hidden="true">&rarr;</span>
-//                             </Link>
-//                         ): (
-//                                 <Link href={route('login')} className="font-semibold leading-relaxed tracking-tighter text-foreground">
-//                                     Log in <span aria-hidden="true">&rarr;</span>
-//                                 </Link>
-//                         )}
-
-//                     </div>
-
-//                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-//                         {auth ? (
-//                             <Link href={route('dashboard')} className="text-base font-semibold leading-relaxed tracking-tighter text-foreground">
-//                                 Dashboard <span aria-hidden="true">&rarr;</span>
-//                             </Link>
-//                         ) : (
-//                                 <Link href={route('login')} className="text-base font-semibold leading-relaxed tracking-tighter text-foreground">
-//                                 Log in <span aria-hidden="true">&rarr;</span>
-//                             </Link>
-//                         )}
-
-//                     </div>
-//                 </nav>
-//             </header>
-
-//             <main className="isolate">
-//                 <div className="relative pt-14">
-//                     <div
-//                         className="absolute inset-x-0 overflow-hidden -top-40 -z-10 transform-gpu blur-3xl sm:-top-80"
-//                         aria-hidden="true"
-//                     >
-//                         <div
-//                             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-red-400 to-red-600 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-//                             style={{
-//                                 clipPath:
-//                                     'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-//                             }}
-//                         />
-//                     </div>
-//                     <div className="py-24 sm:py-32">
-//                         <div className="px-6 mx-auto max-w-7xl lg:px-8">
-//                             <div className="max-w-2xl mx-auto text-center">
-//                                 <h1 className="text-4xl font-bold leading-relaxed tracking-tight text-foreground sm:text-6xl">
-//                                     Get Merchandise in your hands
-//                                 </h1>
-//                                 <p className="mt-6 text-lg leading-8 text-muted-foreground">
-//                                     Riftasks is a simple and easy to use task manager that helps you stay on top of your
-//                                     to-dos.
-//                                 </p>
-//                                 <div className="flex items-center justify-center mt-10 gap-x-6">
-//                                     <Button variant="red" asChild>
-//                                         <Link href={route('login')}>Get started</Link>
-//                                     </Button>
-//                                     <Button variant="ghost" asChild>
-//                                         <Link href={route('login')}>
-//                                             Learn more <span aria-hidden="true">→</span>
-//                                         </Link>
-//                                     </Button>
-//                                 </div>
-//                                 <div className="flex justify-center items-center md:mt-0 pt-12" data-aos="fade-up" data-aos-delay="500">
-//                                     <a href="#competition" className="flex flex-row gap-2 items-center text-gray-600">
-//                                         Scroll down to Explore more
-//                                         <ArrowDownIcon className="w-6 h-6 animate-bounce" />
-//                                     </a>
-//                                 </div>
-//                             </div>
-//                             <div className="flow-root mt-16 sm:mt-24">
-//                                 <div className="p-2 -m-2 rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-//                                     <img
-//                                         src="/assets/images/app.png"
-//                                         alt="App screenshot"
-//                                         width={2432}
-//                                         height={1442}
-//                                         className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
-//                                     />
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                     <div
-//                         className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-//                         aria-hidden="true"
-//                     >
-//                         <div
-//                             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-red-400 to-red-500 opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-//                             style={{
-//                                 clipPath:
-//                                     'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-//                             }}
-//                         />
-//                     </div>
-//                 </div>
-//             </main>
-//         </div>
-//     );
-// }
