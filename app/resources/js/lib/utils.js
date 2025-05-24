8da639047fec786b43be0ab96fd72cfa58863877
PutuRifkiDy <puturifki56@gmail.com>
2025-05-24 13:14:15 +0800

    Added: ErrorHandling

--- app/resources/js/lib/utils.js
+++ app/resources/js/lib/utils.js
@@ -23,3 +23,36 @@ export const STATUS_USER = {
 export function flashMessage(params) {
     return params.props.flash_message;
 }
+
+export const messages = {
+    503: {
+        title: 'Service Unavailable',
+        description: 'Sorry, we are doing some maintenance. Please try again later.',
+        status: '503'
+    },
+    500: {
+        title: 'Server Error',
+        description: 'Oops, something went wrong on our servers. Please try again later.',
+        status: '500'
+    },
+    404: {
+        title: 'Not Found',
+        description: 'Sorry, the page you are looking for could not be found.',
+        status: '404'
+    },
+    403: {
+        title: 'Forbidden',
+        description: 'Sorry, you are forbidden from accessing this page.',
+        status: '403'
+    },
+    401: {
+        title: 'Unauthorized',
+        description: 'Sorry, you are not authorized to access this page.',
+        status: '401'
+    },
+    429: {
+        title: 'Too Many Requests',
+        description: 'Please try again in just a few minutes.',
+        status: '429'
+    },
+}
