118c067fc65e07befb21c7b5476cf4f2c7b7c6bf
PutuRifkiDy <puturifki56@gmail.com>
2025-06-01 14:23:24 +0800

    Refactoring Reset Password

--- app/resources/js/Pages/Auth/ResetPassword.jsx
+++ app/resources/js/Pages/Auth/ResetPassword.jsx
@@ -1,3 +1,4 @@
+import { LineOrnamenIcon, SideRightCrossIcon, SideRightDotIcon } from '@/Components/IconGuest';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import TextInput from '@/Components/TextInput';
@@ -27,27 +28,32 @@ export default function ResetPassword({ token, email }) {
             {/* Template Login */}
             <div className="flex flex-row items-center justify-center">
                 <div className="min-h-full min-w-full">
-                    <div className="flex flex-row rounded-[28px] p-5 border-[1px] shadow md:mx-36">
-                        <div className="hidden gap-5 rounded-[28px] bg-[#4880FF] px-10 py-10 md:flex md:flex-col">
-                            <p className="w-[500px] text-[32px] font-bold text-[#FFFFFF]">
-                                Very good events are waiting for you
+                    <div className="flex flex-row rounded-[28px] p-5 border-[1px] shadow md:mx-36 md:mt-28 mt-16 md:mb-16 mb-10">
+                        <div className="relative hidden gap-5 rounded-l-[28px] bg-gradient-to-r from-[#0F114C] to-[#00658F] px-10 py-10 md:flex md:flex-col w-full dark:border-r-[1px] border-gray-100 dark:bg-none">
+                            <p className="md:w-[330px] text-[32px] font-bold text-[#FFFFFF]">
+                                Very good events are waiting for you Register Now
                             </p>
-                            <img src="/assets/images/image_for_auth.png" alt="" className="h-[379px] w-[354px]" />
+                            <div className='absolute top-24'>
+                                <SideRightDotIcon />
+                            </div>
+                            <div className='absolute rotate-90 right-0 top-24'>
+                                <SideRightCrossIcon />
+                            </div>
+                            <div className='absolute bottom-10 right-0'>
+                                <LineOrnamenIcon />
+                            </div>
+                            <img src={`${window.location.origin}/assets/images/landing/icon-maskot-itcc.png`} alt="" className="absolute left-24 bottom-24 h-auto md:w-[280px]" />
                         </div>
 
                         <div className="w-full">
-                            <Card className="flex flex-col border-none py-24 shadow-none">
+                            <Card className="flex flex-col border-none py-24 shadow-none dark:bg-[#040529] min-h-[650px]">
                                 <CardHeader className="flex flex-col items-center justify-center">
                                     <Link
                                         href="#"
-                                        className="text-[25px] font-bold leading-relaxed tracking-tighter text-[#4880FF]"
+                                        className="text-[25px] font-bold leading-relaxed tracking-tighter text-[#0F114C] dark:text-white"
                                     >
                                         Reset Password
                                     </Link>
-                                    {/* <h2 className=" text-md font-medium leading-relaxed tracking-tight text-center text-muted-foreground w-[400px]">
-                                        Forgot your password? No problem. Just let us know your email address and we will email you a password
-                                        reset link that will allow you to choose a new one.
-                                    </h2> */}
                                 </CardHeader>
                                 <CardContent>
                                     <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -59,7 +65,7 @@ export default function ResetPassword({ token, email }) {
                                                     type="email"
                                                     name="email"
                                                     value={data.email}
-                                                    className="mt-1 block w-full rounded-[10px] border-[1px] border-[#818181] px-4 placeholder:text-[14px] placeholder:text-[#6F6F6F]"
+                                                    className="mt-1 block w-full rounded-[10px] border-[1px] border-[#818181] px-4 placeholder:text-[14px] placeholder:text-[#6F6F6F] dark:text-white dark:bg-[#040529]"
                                                     placeholder="Insert your email address"
                                                     autoComplete="username"
                                                     isFocused={true}
@@ -80,7 +86,7 @@ export default function ResetPassword({ token, email }) {
                                                     type="password"
                                                     name="password"
                                                     value={data.password}
-                                                    className="mt-1 block w-full rounded-[10px] border-[1px] border-[#818181] px-4 placeholder:text-[14px] placeholder:text-[#6F6F6F]"
+                                                    className="mt-1 block w-full rounded-[10px] border-[1px] border-[#818181] px-4 placeholder:text-[14px] placeholder:text-[#6F6F6F] dark:text-white dark:bg-[#040529]"
                                                     placeholder="Insert your new password"
                                                     autoComplete="new-password"
                                                     isFocused={true}
@@ -101,7 +107,7 @@ export default function ResetPassword({ token, email }) {
                                                     id="password_confirmation"
                                                     name="password_confirmation"
                                                     value={data.password_confirmation}
-                                                    className="mt-1 block w-full rounded-[10px] border-[1px] border-[#818181] px-4 placeholder:text-[14px] placeholder:text-[#6F6F6F]"
+                                                    className="mt-1 block w-full rounded-[10px] border-[1px] border-[#818181] px-4 placeholder:text-[14px] placeholder:text-[#6F6F6F] dark:text-white dark:bg-[#040529]"
                                                     placeholder="Confirm your new password"
                                                     autoComplete="new-password"
                                                     onChange={(e) => setData('password_confirmation', e.target.value)}
