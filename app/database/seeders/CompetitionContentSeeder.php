3263bd8906ef467ab1dbdbcb594ff237b58e2817
PutuRifkiDy <puturifki56@gmail.com>
2025-06-27 16:23:00 +0800

    Update: Guidebook Link

--- app/database/seeders/CompetitionContentSeeder.php
+++ app/database/seeders/CompetitionContentSeeder.php
@@ -16,7 +16,7 @@ class CompetitionContentSeeder extends Seeder
                 'competition_id'   => '1',
                 'location'         => 'Online',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/1gpNuQSkSava0W9d2pqKs4Pe__zjQOUTw/view?usp=drivesdk',
+                'guidebook_link'   => 'https://drive.google.com/file/d/1geiD9GmFW-suDWB--cgpSc5d6G1OGI2T/view?usp=sharing',
             ],
             [
                 'competition_id'   => '2',
@@ -28,19 +28,19 @@ class CompetitionContentSeeder extends Seeder
                 'competition_id'   => '3',
                 'location'         => 'Semi-Offline',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/1qQb4q5Z2XisyWKFg78G2UornKyTok7GL/view?usp=drivesdk',
+                'guidebook_link'   => 'https://drive.google.com/file/d/1tPTn4cIR_bRDy5pCfa9CVqezYN_Smyd4/view?usp=sharing',
             ],
             [
                 'competition_id'   => '4',
                 'location'         => 'Online',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/1uMgngGYez3Vickh20CfBVkAFxkGrOzpf/view?usp=drivesdk',
+                'guidebook_link'   => 'https://drive.google.com/file/d/1NsSIRd7c-PR4Zc2NOnZBVl5q7BOV2cAt/view?usp=sharing',
             ],
             [
                 'competition_id'   => '5',
                 'location'         => 'Semi-Offline',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/1bt6dpZSmobpeUtlEvdsBGof7FlXXikm8/view?usp=drivesdk',
+                'guidebook_link'   => 'https://drive.google.com/file/d/1BM-Ct7g7104Y-c-L_SVAVP-xvAJlkieZ/view?usp=drive_link',
             ],
             [
                 'competition_id'   => '6',
@@ -52,31 +52,31 @@ class CompetitionContentSeeder extends Seeder
                 'competition_id'   => '7',
                 'location'         => 'Offline',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/13G7soQiihSm6C8tmT2OB-O3YjFLyZYtm/view?usp=drivesdk',
+                'guidebook_link'   => 'https://drive.google.com/file/d/1yTwXkeWvqQFimznis5woRqUrzTtbPh0-/view?usp=sharing',
             ],
             [
                 'competition_id'   => '8',
                 'location'         => 'Online',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/1Kv3NNVSHkRUXL4PbloPNT8oIm6UuoBEc/view?usp=drive_link',
+                'guidebook_link'   => 'https://drive.google.com/file/d/1okwV-M5U6YxKAViU-SnYLt3ayg_bXIft/view?usp=sharing',
             ],
             [
                 'competition_id'   => 9,
                 'location'         => 'Semi-Offline',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/1MW9GnshOnbbs5ty_KbOyWTvfAV5EtAZP/view?usp=drive_link',
+                'guidebook_link'   => 'https://drive.google.com/file/d/1okwV-M5U6YxKAViU-SnYLt3ayg_bXIft/view?usp=sharing',
             ],
             [
                 'competition_id'   => 10,
                 'location'         => 'Online',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/1DpOsMx79OsYHV52pdymfdBzHfyKTHIz4/view?usp=drivesdk',
+                'guidebook_link'   => 'https://drive.google.com/drive/folders/1xB4ELL6xZc70YtebdyQXBkT6cTJ5k1X8?usp=sharing',
             ],
             [
                 'competition_id'   => 11,
                 'location'         => 'Online',
                 'how_to_join_link' => 'https://itcc.hmtiudayana.id/coming-soon',
-                'guidebook_link'   => 'https://drive.google.com/file/d/1c47sUere9YwCLpqv8XinuteRTSp3bEme/view?usp=drivesdk',
+                'guidebook_link'   => 'https://drive.google.com/drive/folders/1SR8xUyvpefDjrJqwJy6ktrpciFqaoWi2?usp=sharing',
             ],
         ];
 
